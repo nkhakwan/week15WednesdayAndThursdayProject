bac06dd9ebeddbf36e81ac9e2c481918f057648b
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-06-23 15:07:02 -0700

    stores tree, shrub, flower values

--- src/main.js
+++ src/main.js
@@ -11,22 +11,87 @@ $(document).ready(function () {
   const flower = storeState(initialPlantState);
   
   $("#feed").click(function () {
+    var radioValue = $("input[name='plant-choice']:checked").val();
 
-    const thisTree = tree(blueFood);
-    $("#soil-value").text(thisTree.soil);
+    console.log(radioValue);
+
+    
+    let temp;
+
+    switch (radioValue) {
+      case "tree":
+        temp = tree;
+      break;
+      case "shrub":
+        temp = shrub;
+      break;
+      case "flower":
+        temp = flower;
+      break;
+      default:
+      break;
+    }
+    
+    const thisPlant = temp(blueFood);
+
+    $("#"+ radioValue + "-soil-value").text(thisPlant.soil);
   });
 
   $("#hydrate").click(function () {
+    var radioValue = $("input[name='plant-choice']:checked").val();
+    console.log(radioValue);
+    // const thisTree = tree(hydrate);
+    // $("#tree-water-value").text(thisTree.water);
 
-    const thisTree = tree(hydrate);
-    $("#water-value").text(thisTree.water);
+
+    let temp;
+
+    switch (radioValue) {
+      case "tree":
+        temp = tree;
+      break;
+      case "shrub":
+        temp = shrub;
+      break;
+      case "flower":
+        temp = flower;
+      break;
+      default:
+      break;
+    }
+    
+    const thisPlant = temp(hydrate);
+
+    $("#"+ radioValue + "-water-value").text(thisPlant.water);
+
+
     // const newState = stateChanger(hydrate);
     // $("#water-value").text(newState.water);
   });
 
   $("#giveLight").click(function () {
-    const thisTree = tree(giveLight);
-    $("#light-value").text(thisTree.light);
+    var radioValue = $("input[name='plant-choice']:checked").val();
+    console.log(radioValue);
+    // const thisTree = tree(giveLight);
+    // $("#tree-light-value").text(thisTree.light);
+    let temp;
 
+    switch (radioValue) {
+      case "tree":
+        temp = tree;
+      break;
+      case "shrub":
+        temp = shrub;
+      break;
+      case "flower":
+        temp = flower;
+      break;
+      default:
+      break;
+    }
+    
+    const thisPlant = temp(giveLight);
+
+    $("#"+ radioValue + "-light-value").text(thisPlant.light);
   });
 });
